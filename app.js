ac19046a80d7866c950f0a0e23474835a5952e5b
taekhun <nth9708@naver.com>
2020-12-30 11:48:04 +0900

    #2.16 Loacal Variables in Pug

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 // const express = require('express');
 // // require => express를 내 파일 내에서 찾고 -> node_module에서 찾음
 
 const app = express();
-
-
 
 // Middlewares (top-bottom)
 app.use(helmet()); // 보안
@@ -21,6 +20,8 @@ app.use(cookieParser()); // 쿠키관리
 app.use(bodyParser.json()); // request의 form, json 관리
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev")); // 접속한 log관리
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter); // "/~"에 접속하면 => globalRouter에서 찾음
 app.use(routes.users, userRouter); // "/users/"에 접속하면 => userRouter에서 찾음
---
+++ middlewares.js
@@ -0,0 +1,10 @@
+//local 변수, routes변수를 담는 곳
+//next()를 통해 다음 middleware(다음 줄)로 넘겨야 함
+
+import routes from "./routes";
+
+export const localsMiddleware = (req, res, next) => {
+    res.locals.siteName = "take-tube";
+    res.locals.routes = routes;
+    next();
+}
