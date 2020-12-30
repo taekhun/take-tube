4a09425f4b37ef903260a350a846c14bc275b90c
taekhun <nth9708@naver.com>
2020-12-30 12:17:49 +0900

    2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,19 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+//1st arg : 렌더링 할 템플릿
+//2nd arg : 템플릿에 추가할 정보가 담긴 객체
+
+export const home = (req, res) => res.render("home", {pageTitle : "Home"});
+
+export const search = (req, res) => {
+    const {query: {term : searchingBy}} = req;
+    //const term = req.query.term
+    res.render("search", {pageTitle : "Search", searchingBy : searchingBy});
+    //res.render("search", {pageTitle : "Search", searchingBy});
+}
+
+export const upload = (req, res) => res.render("upload", {pageTitle : "Upload"});
+
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle : "Video Title"});
+
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle : "Edit Detail"});
+
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle : "Delete Video"});
--- note.md
+++ note.md
@@ -5,13 +5,10 @@ View : 화면 단 (PUG)
 Controller : 데이터를 다루는 함수, 라우팅 함수에서 실행하는 함수.
 
 # 2.13
-
 - app.set("view engine", "pug") => view engine 설정
-
 - PUG
 
 <h1>Hello</h1> => h1 Hello
-
 - res.render("home");
   /views 폴더 안에 있는 home.pug를 렌더링해줌
   따라서 pug를 사용할 땐, /views 폴더를 만들고 사용해야함.
@@ -20,5 +17,29 @@ Controller : 데이터를 다루는 함수, 라우팅 함수에서 실행하는 
   Layout 전체를 복사
   block ~ 식으로 원하는대로 바꿔서 씀
 
+# 2.16, 17 - Local Varibles in Pug
+app.use("middlewares")
+미들웨어를 사용하여, 그 안에서 전역 변수를 담아서 사용
+반드시 next()를 해줘야 다음 미들웨어로 넘어감 !
 
-//2.16 차례
+ex)
+res.locals.siteName = "take-tube";
+res.locals.routes = routes;
+
+in /views
+import routes from ./routes
+..
+#{siteName}
+route.join
+
+# 2.18 
+const term = req.query.term  
+=> const {query:{term}} = req; 
+
+**queryString에 대하여..**
+
+사용자가 입력 데이터를 전달하는 방법중의 하나로, url 주소에 미리 협의된 데이터를 파라미터를 통해 넘기는 것을 말한다.
+
+1. HTML(PUG)에서 form + input 으로 정보를 보낸다. 여기서 form태그는 get방식이어야하며, input태그의 name값이 쿼리이름이 된다.
+2. Controller에서, req.query << 로 input의 name값으로 보낸 데이터를 받는다.
+3. 그리고 위 데이터를 다시 PUG로 전달해서 받아준다.
