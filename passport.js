53cedb3f59999315111b3af5730fabbf64174254
taekhun <nth9708@naver.com>
2021-01-09 16:22:57 +0900

    bug fixed

--- passport.js
+++ passport.js
@@ -34,10 +34,5 @@ passport.use(
   )
 );
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
